6dd571cd270c0f22fd0e615457d2a049de3d2f97
Majenko <MarioSchrenk@t-online.de>
2012-03-23 15:03:45 +0100

    update, maybe not work

--- OLS_NG/src/Logic_Sniffer.vhd
+++ OLS_NG/src/Logic_Sniffer.vhd
@@ -2,7 +2,7 @@
 -- Logic_Sniffer.vhd
 --
 -- Copyright (C) 2006 Michael Poppitz
--- Copyright (C) 2011 Mario Schrenk
+-- Copyright (C) 2011-2012 Mario Schrenk
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -44,18 +44,20 @@ generic (
     CLOCK_SPEED             : integer := 50
    );
 port (
-    ResetSwitch_ni      : in  std_logic;
-    XtalClock_i         : in  std_logic;
-    XtalEnable_o        : out std_logic;
-    
-    input               : in  std_logic_vector(31 downto 1);
-    J2                  : in  std_logic_vector(7 downto 0);
+    RESET_i         : in  std_logic;
+    CLK_i           : in  std_logic;
 
-    rx                  : in  std_logic;
-    tx                  : inout std_logic;
-    
-    led                 : out std_logic_vector(7 downto 0);
-    switch              : in  std_logic_vector(7 downto 0);
+    ExtClk_i        : in  std_logic;
+    ExtTrigger_i    : in  std_logic;
+    ExtInside_i     : in  std_logic_vector(31 downto 0);
+    ExtOutside_i    : in  std_logic_vector(31 downto 0);
+
+    RS232_Speed_i   : in  std_logic_vector(1 downto 0);
+    RS232_XonXoff_i : in  std_logic; 
+    RS232_Rx_i      : in  std_logic;
+    RS232_Tx_o      : out std_logic;
+
+    LED_no          : out std_logic_vector(7 downto 0);
 
     -- ===  SRAM  === --
     SRAM_CE_no      : out   std_logic;                                                              -- Chip Enable (low active)
@@ -69,13 +71,6 @@ end Logic_Sniffer;
 --**************************************************************************************************
 ARCHITECTURE rtl OF Logic_Sniffer IS
 --**************************************************************************************************
-
-component clockman
-port (
-    clkin : in  std_logic;
-    clk0 : out std_logic
-    );
-end component;
 
 
 component eia232
@@ -121,7 +116,7 @@ port (
     memoryOut       : out std_logic_vector(31 downto 0);
     memoryRead      : out std_logic;
     memoryWrite     : out std_logic;
-    extTriggerIn    : in  std_logic;
+    ExtTriggerIn    : in  std_logic;
     extTriggerOut   : out std_logic;           
     extClockOut     : out std_logic;
     armLED          : out std_logic;
@@ -146,14 +141,12 @@ port (
     );
 end component;
     
-signal resetSwitch              : std_logic := '0';
-signal ls_BaudRate              : std_logic_vector(1 downto 0) := "10"; --Set baud rate to 115000 bps
 signal cmd                      : std_logic_vector(39 downto 0);
 signal memoryIn                 : std_logic_vector(31 downto 0);
 signal memoryOut                : std_logic_vector(31 downto 0);
 signal ls_input                 : std_logic_vector(31 downto 0);
 signal output                   : std_logic_vector(31 downto 0);
-signal clock                    : std_logic;
+-- signal clock                    : std_logic;
 -- signal clk                      : std_logic;
 signal read                     : std_logic; 
 signal write                    : std_logic; 
@@ -162,30 +155,30 @@ signal send                     : std_logic;
 signal busyReg                  : std_logic; 
 signal testModeReg              : std_logic := '0'; 
 signal numberSchemeReg          : std_logic := '1'; 
-signal testmodeState            : std_logic := '0';  --Puts the OLS into a test mode. The Wing connector outputs a test pattern that can be captured by the Buffered connector with a ribbon cable.
-signal numbering                : std_logic := '0';      --Selects the number scheme on the OLS. 0 is Inside scheme 1 is Outside scheme
+-- signal testmodeState            : std_logic := '0';  --Puts the OLS into a test mode. The Wing connector outputs a test pattern that can be captured by the Buffered connector with a ribbon cable.
+-- signal numbering                : std_logic := '0';      --Selects the number scheme on the OLS. 0 is Inside scheme 1 is Outside scheme
 signal ti_eia232_ts_LED_Xon     : std_logic;
 signal ti_eia232_ts_LED_Xoff    : std_logic;
 signal test_counter             : unsigned(31 downto 0) := (others => '0');
 
 
 
-signal extClockIn               : std_logic;
-signal extTriggerIn             : std_logic;
+-- signal ExtClk_i                 : std_logic;
+-- signal ExtTrigger_i             : std_logic;
 signal extClockOut              : std_logic;
 signal extTriggerOut            : std_logic;
 signal armLED                   : std_logic;
 signal triggerLED               : std_logic;
 signal li_Core_ls_intReset      : std_logic;
 
-signal ls_J24_input             : std_logic_vector(3 downto 0);
+-- signal ls_J24_input             : std_logic_vector(3 downto 0);
 signal ti_Status_LED_ts_Status_LED  : std_logic;
 
 
-signal ti_n2m_Translate_ls_WriteDataStrobe  : std_logic;
-signal ti_n2m_Translate_ls_WriteData        : std_logic_vector(35 downto 0);
-signal ti_n2m_Translate_ls_ReadDataStrobe   : std_logic;
-signal ti_SRAM_ls_output                    : std_logic_vector(35 downto 0);
+-- signal ti_n2m_Translate_ls_WriteDataStrobe  : std_logic;
+-- signal ti_n2m_Translate_ls_WriteData        : std_logic_vector(35 downto 0);
+-- signal ti_n2m_Translate_ls_ReadDataStrobe   : std_logic;
+-- signal ti_SRAM_ls_output                    : std_logic_vector(35 downto 0);
 
 
 
@@ -201,18 +194,13 @@ constant RATE       : integer := 230400;            -- maximum & base rate
 BEGIN
 --**************************************************************************************************
 
-    -------------------------------------------------
+    -- -----------------------------------------------
     -- Abbildungen Extern -> Intern
-    -------------------------------------------------
+    -- -----------------------------------------------
 
-    b_MapExt2Int: block
-    begin
-        resetSwitch     <= not(ResetSwitch_ni);
-        ls_BaudRate     <= switch(1 downto 0);                                  -- SW1-1 und SW1-2
-        extClockIn      <= XtalClock_i;             -- J2(1);
-        extTriggerIn    <= not(switch(4));          -- J2(3);                   -- SW3
-        ls_J24_input <= J2(2) & J2(4) & J2(6) & J2(7);
-    end block b_MapExt2Int;
+    -- b_MapExt2Int: block
+    -- begin
+    -- end block b_MapExt2Int;
 
 
     -- -------------------------------------------------
@@ -230,57 +218,46 @@ BEGIN
 
     b_MapInt2Ext: block
     begin
-        XtalEnable_o  <= '1';
-
-        -- dataReady <= busyReg;
-
-        led(7) <= armLED;
-        led(6) <= triggerLED;
-        led(5) <= '1';
-        led(3) <= not(ti_eia232_ts_LED_Xoff);
-        led(4) <= not(ti_eia232_ts_LED_Xon); 
-        led(2) <= not(tx);
-        led(1) <= not(rx);
-        led(0) <= not(ti_Status_LED_ts_Status_LED);
+        LED_no(7) <= armLED;
+        LED_no(6) <= triggerLED;
+        LED_no(5) <= not('0');
+        LED_no(3) <= not(ti_eia232_ts_LED_Xoff);
+        LED_no(4) <= not(ti_eia232_ts_LED_Xon); 
+        LED_no(2) <= not('0');
+        LED_no(1) <= not('0');
+        LED_no(0) <= not(ti_Status_LED_ts_Status_LED);
     end block b_MapInt2Ext;
 
+    
    -- ===  Multiplexer to connect test mode and number schemes  === --
-    process(resetSwitch, clock)
+    process(RESET_i, CLK_i)
     begin
-        if (resetSwitch = '1') then
+        if (RESET_i = '1') then
             ls_input        <= (others => '0');
             test_counter    <= (others => '0');
-        elsif rising_edge(clock) then
+        elsif rising_edge(CLK_i) then
             if (testModeReg = '1') then 
                 -- ===  Test Mode  === --
-               -- ls_input     <= "000000000000000000000000" & std_logic_vector(test_counter(14 downto 7));
                ls_input     <= "0000000" & std_logic_vector(test_counter(31 downto 7));
                -- ls_input     <= std_logic_vector(test_counter);
                test_counter <= test_counter + 1;
             else 
                 if (numberSchemeReg = '1') then
                     -- ===  Outside Number Scheme  === --
-                    ls_input(31 downto 4) <= not(switch(5)) & input(31 downto 5);            -- SW4 & J1 (teilweise)
-                    ls_input( 3 downto 0) <= ls_J24_input;                                   -- Eingabe mit Stiftleise J2 �berschreiben
+                    ls_input <= ExtOutside_i;
                 else
                     -- ===  Inside Number Scheme  === --
-                    ls_input(31 downto 0) <= not(switch(5)) & input(31 downto 1);            -- SW4 & J1
+                    ls_input <= ExtInside_i;
                 end if;
             end if;
         end if;
     end process;
 
 
-    i_clockman: clockman 
-    port map (
-        clkin => XtalClock_i,
-        clk0  => clock
-        );
-
     i_Status_LED: entity work.Status_LED
     port map (
-        RESET_i              => resetSwitch,                                                        -- Reset      (high active)
-        CLK_i                => clock,                                                              -- Systemtakt (132,7104 MHz)            -- blinkt viel langsamer ;-) (sp�ter noch anpassen)
+        RESET_i              => RESET_i,                                                            -- Reset      (high active)
+        CLK_i                => CLK_i,                                                              -- Systemtakt (132,7104 MHz)            -- blinkt viel langsamer ;-) (sp�ter noch anpassen)
 
         slow_i               => '0',                                                                -- Blinken im Halbsekundentakt
         fast_i               => '1',                                                                -- sehr schnelles Blinken
@@ -295,12 +272,12 @@ BEGIN
         RATE  => RATE
         )
     port map (
-        clock       => clock, 
-        reset       => resetSwitch,
-        speed       => switch(1 downto 0),          -- SPEED
-        XOnOff_i    => switch(3),                   -- enable Xon/Xoff
-        rx          => rx,
-        tx          => tx,
+        clock       => CLK_i, 
+        reset       => RESET_i,
+        speed       => RS232_Speed_i,                       -- SPEED
+        XOnOff_i    => RS232_XonXoff_i,                     -- enable Xon/Xoff
+        rx          => RS232_Rx_i,
+        tx          => RS232_Tx_o,
         cmd         => cmd,
         execute     => execute,
         data        => output,
@@ -315,13 +292,13 @@ BEGIN
         MEMORY_DEPTH => MEMORY_DEPTH
     )
     port map (
-        clock           => clock,
-        extReset        => resetSwitch,
+        clock           => CLK_i,
+        extReset        => RESET_i,
         cmd             => cmd,
         execute         => execute,
         intReset        => li_Core_ls_intReset,
         input           => ls_input,
-        inputClock      => extClockIn,
+        inputClock      => ExtClk_i,
         -- sampleReady50   => ready50,
         output          => output,
         outputSend      => send,
@@ -330,7 +307,7 @@ BEGIN
         memoryOut       => memoryOut,
         memoryRead      => read,
         memoryIn        => memoryIn,
-        extTriggerIn    => extTriggerIn,
+        ExtTriggerIn    => ExtTrigger_i,
         extTriggerOut   => extTriggerOut,
         extClockOut     => extClockOut,
         armLED          => armLED,
@@ -339,9 +316,9 @@ BEGIN
         testMode        => testModeReg
         );
 
-    -- process(clock)
+    -- process(CLK_i)
     -- begin
-        -- if rising_edge(clock) then
+        -- if rising_edge(CLK_i) then
             -- write_p0 <= write;
         -- end if;
     -- end process;
@@ -353,7 +330,7 @@ BEGIN
         -- )
     -- port map (
         -- RESET_i             => li_Core_ls_intReset,                         -- Reset (high active)
-        -- CLK_i               => clock,                                       -- Systemtakt
+        -- CLK_i               => CLK_i,                                       -- Systemtakt
 
         -- -- ===  Core-Interface  === --
         -- WriteDataStrobe_i   => write,
@@ -377,9 +354,9 @@ BEGIN
         -- MEMORY_DEPTH => Physical_MEMORY_DEPTH
         -- )
     -- port map (
-        -- reset   => resetSwitch,
+        -- reset   => RESET_i,
         -- -- reset   => li_Core_ls_intReset,
-        -- clock   => clock,
+        -- clock   => CLK_i,
         -- write   => ti_n2m_Translate_ls_WriteDataStrobe,
         -- input   => ti_n2m_Translate_ls_WriteData,
         -- read    => ti_n2m_Translate_ls_ReadDataStrobe,
@@ -393,8 +370,8 @@ BEGIN
         gData_Width     =>  8
         )
     port map (
-        Reset_i         => resetSwitch,
-        CLK_i           => clock,
+        Reset_i         => RESET_i,
+        CLK_i           => CLK_i,
     
         Write_i         => write,
         Data_i          => memoryOut(7 downto 0),
